f81bc2f64393a0f56ded7419a13120401a28577b
Bartosz Nowacki <bartosz.nowacki2@@gmail.com>
2024-01-22 20:14:33 +0100

    add JwtTokenFilter and configuration

---
+++ auth-service/src/main/java/com/bartosznowacki/app/authservice/security/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.bartosznowacki.app.authservice.security;
+
+import com.bartosznowacki.app.authservice.user.SecurityUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final SecurityUserRepository repository;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> repository.findSecurityUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
---
+++ auth-service/src/main/java/com/bartosznowacki/app/authservice/security/JwtTokenFilter.java
@@ -0,0 +1,65 @@
+package com.bartosznowacki.app.authservice.security;
+
+import com.bartosznowacki.app.authservice.jwt.IJwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+@RequiredArgsConstructor
+class JwtTokenFilter extends OncePerRequestFilter {
+    private final IJwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull jakarta.servlet.http.HttpServletResponse response,
+                                    @NonNull jakarta.servlet.FilterChain chain
+    ) throws jakarta.servlet.ServletException, IOException {
+        if (validateTokenHeader(request) && validateToken(request)) {
+            applyAuthenticationToSecurityContext(request);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean validateTokenHeader(jakarta.servlet.http.HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .map(notNullHeader -> notNullHeader.startsWith("Bearer "))
+                .orElse(false);
+    }
+
+    private boolean validateToken(jakarta.servlet.http.HttpServletRequest request) {
+        final String jwt = getJwt(request);
+        return jwtService.validate(jwt);
+    }
+
+    private void applyAuthenticationToSecurityContext(HttpServletRequest request) {
+        final String jwt = getJwt(request);
+        UserDetails userDetails = userDetailsService
+                .loadUserByUsername(jwtService.extractUsername(jwt));
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails == null ? List.of() : userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String getJwt(jakarta.servlet.http.HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .map(header -> header.split(" ")[1].trim())
+                .orElse(null);
+    }
+}
